f236f1e5190a180cfe9105f9409fc8633f6c036b
shaungohsc <39594827+shaungohsc@users.noreply.github.com>
2018-06-21 17:30:55 +0800

    HomePage Interface
    -Datepicker
    -Drop down for pax and Venue-type

--- NightlifeChope/client/main.js
+++ NightlifeChope/client/main.js
@@ -3,17 +3,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './html/main.html';
 
-/*
-Meteor.startup(function(){
-    Router.addRoute('/home', 'homeTemplate');
-    Router.addRoute('/admin', 'adminTemplate');
-    Router.addRoute('/faq', 'faqTemplate');
-    Router.addRoute('/contact', 'contactTemplate');
-
-    Router.run();
+Meteor.startup(() => {
 });
 
-*/
-Meteor.startup(() => {
-  // code to run on server at startup
+Template.homeTemplate.onRendered(function() {
+  $('select').material_select();
 });
--- NightlifeChope/server/main.js
+++ NightlifeChope/server/main.js
@@ -2,25 +2,5 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 Meteor.startup(() => {
-  // code to run on server at startup
-  //Session.setDefault("templateName", "homeTemplate")
-});
-/*
-Template.body.helpers({
-  template_name(){
-    return Session.get("templateName")
-  }
-});
 
-Template.body.events({
-  'click .faq'() {
-    Session.set("templateName", "faqTemplate");
-  },
-  'click .contact'() {
-     Session.set("templateName", "contactTemplate");
-  },
-  'click .admin'() {
-     Session.set("templateName", "adminTemplate");
-  }
 });
-*/
