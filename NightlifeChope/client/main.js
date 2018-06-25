e99e0e66e5200bb6d5983ca1bc3afa3849b229e9
shaungohsc <39594827+shaungohsc@users.noreply.github.com>
2018-06-25 18:50:19 +0800

    BUG FIXES!! Homepage header layout bug, some CSS changes

--- NightlifeChope/client/main.js
+++ NightlifeChope/client/main.js
@@ -12,11 +12,3 @@ import '../imports/startup/accounts-config.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
-
-ServiceConfiguration.configurations.upsert(
-  {
-    $set: {
-      loginStyle: "redirect",
-    }
-  }
-);
